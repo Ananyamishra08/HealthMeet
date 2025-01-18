74920296ea49f3dc3361fc977f21c71e6ab9bcef
Ananya Mishra <98649189+Ananyamishra08@users.noreply.github.com>
2025-01-18 23:05:04 +0530

    Update AdminContext.jsx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -7,7 +7,7 @@ export const AdminContext = createContext()
 
 const AdminContextProvider = (props) => {
 
-    export const backendUrl = "https://healthmeet.onrender.com"
+    const backendUrl = "https://healthmeet.onrender.com"
 
     const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '')
 
